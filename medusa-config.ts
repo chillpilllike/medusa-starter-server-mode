fec06519ab4c85179ba4d8ce6b45f3b3d38eec14
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-20 18:28:08 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -8,6 +8,7 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL, // Added redisUrl
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server", // Added workerMode
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
